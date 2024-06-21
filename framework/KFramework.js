c2cef40156fb1eeabce4ce8c48e0d1eb6cd655c8
Zeeteh <gesellchen.s@icloud.com>
2024-06-21 04:33:04 +0200

    Add files via upload

--- framework/KFramework.js
+++ framework/KFramework.js
@@ -1,54 +1,53 @@
-var VERSION		= '1.1.0';
+var VERSION = '1.1.0';
 
-var KFramework = (new function KFramework() {
-	this.load = [
-		/* Tools */
-		'tools/JSON',
-		'tools/Functions',
-		'tools/Number',
-		'tools/String',
-		'tools/Array',
-		'tools/Object',
-		'tools/User',
-		'tools/Dice',
-		'tools/StringBuffer',
-		
-		/* Core */
-		'core/Hash',
-		'core/Hooks',
-		'core/Database',
-		'core/Logger',
-		'core/Cronjob',
-		'core/Bot',
-		'core/KCode',
-		'core/KBank',
-		'core/Files',		
-		'core/Channel',
-		'core/User',
-		/* 'core/AppStore', */
-		'core/View',
-		'core/Toplist',
-		'core/AppInfo'
-		/* 'core/KConfig' */
-	];
-	
-	for(var entry in this.load) {
-		require('framework/' + this.load[entry] + '.js');
-	}
-	
-	this.startUp = function startUp() {
-		KBank.dataMigration();
-	};
-	
-	this.store = function store() {
-		Cron.saveData();
-	};
-	
-	this.shutDown = function shutDown() {
-		Cron.onShutdown();
-	};
-	
-	this.toString = function toString() {
-		return '[KFramework Core]';
-	};
-}());
+var KFramework = (function KFramework() {
+    this.load = [
+        /* Tools */
+        'tools/Functions',
+        'tools/Number',
+        'tools/String',
+        'tools/Array',
+        'tools/Object',
+        'tools/User',
+        'tools/Dice',
+        'tools/StringBuffer',
+
+        /* Core */
+        'core/Hash', 
+        'core/Hooks',
+        'core/Database',
+        'core/Logger',
+        'core/Cronjob',
+        'core/Bot',
+        'core/KCode',
+        'core/KBank',
+        'core/Files',
+        'core/Channel',
+        'core/User',
+        'core/View',
+        'core/Toplist',
+        'core/AppInfo'
+    ];
+
+    for (var entry in this.load) {
+        require('./framework/' + this.load[entry] + '.js');
+    }
+
+    this.startUp = function startUp() {
+        KBank.dataMigration();
+    };
+
+    this.store = function store() {
+        Cron.saveData();
+    };
+
+    this.shutDown = function shutDown() {
+        Cron.onShutdown();
+    };
+
+    this.toString = function toString() {
+        return '[KFramework Core]';
+    };
+
+    return this;
+})();
